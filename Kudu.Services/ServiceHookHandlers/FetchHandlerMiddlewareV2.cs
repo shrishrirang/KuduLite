5a224ba8245225ed2c6ac0fd1073ed5a6382dc42
Sanchit Mehta <sanchit.mehta602@gmail.com>
2019-06-17 13:08:44 -0700

    Test

--- Kudu.Services/ServiceHookHandlers/FetchHandlerMiddlewareV2.cs
+++ Kudu.Services/ServiceHookHandlers/FetchHandlerMiddlewareV2.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Kudu.Contracts.Settings;
 using Kudu.Contracts.Tracing;
